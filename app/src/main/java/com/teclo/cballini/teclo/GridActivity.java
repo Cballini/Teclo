f6d4d7aa00c348a3ab25ae54e2ca462f1c769455
Cballini <cec.ballini@gmail.com>
2017-09-21 15:35:29 +0200

    Déplacements

--- app/src/main/java/com/teclo/cballini/teclo/GridActivity.java
+++ app/src/main/java/com/teclo/cballini/teclo/GridActivity.java
@@ -1,19 +1,24 @@
 package com.teclo.cballini.teclo;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.view.ViewPropertyAnimatorCompatSet;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
 
-public class GridActivity extends AppCompatActivity implements View.OnClickListener {
+public class GridActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private GridView gridView;
     private GridAdapter gridAdapter;
@@ -21,6 +26,10 @@ public class GridActivity extends AppCompatActivity implements View.OnClickListe
     private int col_MAX = 7;
     private int row_MAX = 9;
     private Piece[][] gridPieces = new Piece[col_MAX][row_MAX];
+    private boolean firstClick = true;
+    private int caseX;
+    private int caseY;
+    private int nPos;
 
 
     @Override
@@ -29,46 +38,61 @@ public class GridActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_grid);
 
         gridView = (GridView) findViewById(R.id.gridGame);
+
         initItems();
         initPieces();
+
         gridAdapter = new GridAdapter(this, R.layout.grid_item, imgItems);
         gridView.setAdapter(gridAdapter);
-
-        /*ImageView im = (ImageView) findViewById(R.id.caseGame);
-        im.setOnClickListener(this);*/
+        gridView.setOnItemClickListener(this);
     }
 
-    public void onClick(View v){
-        Log.i("test", "ok");
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if(firstClick){
+            if (gridPieces[posToY(position)][posToX(position)].getTeam()!=-1) {
+                Log.i("test", "ok");
+                caseX = posToX(position);
+                caseY = posToY(position);
+                firstClick = false;
+            }
+        }
+        else{
+            movement(caseY, caseX, posToY(position), posToX(position));
+            firstClick = true;
+        }
     }
 
+    //grille vide
     private void initItems(){
         imgItems = new ArrayList<>();
         for (int y=0; y<col_MAX; y++){
             for(int x=0; x<row_MAX; x++){
-                Bitmap item = Bitmap.createBitmap(24,24,Bitmap.Config.RGB_565);
-                item.eraseColor(Color.WHITE);
                 gridPieces[y][x] = new Piece();
-                imgItems.add(item);
+                imgItems.add(gridPieces[y][x].getImg());
             }
         }
     }
 
+    //placement des pièces initial
     private void initPieces(){
         initTeamOne();
         initTeamTwo();
         initNeutral();
 
+        updateImgItems();
+    }
+
+    //mise à jour de la grille de bitmap
+    private void updateImgItems(){
         for (int y=0; y<col_MAX; y++){
             for(int x=0; x<row_MAX; x++){
-                if(gridPieces[y][x].getTeam() != -1) {
-                    int nb = col_MAX*x+(y+1);
-                    imgItems.set(col_MAX*x+y, gridPieces[y][x].getImg());
-                }
+                imgItems.set(xyToPos(y,x), gridPieces[y][x].getImg());
             }
         }
     }
 
+    //placement initial des pièces neutres
     private void initNeutral() {
         int mCol = col_MAX/2;
         int mRow = row_MAX/2;
@@ -88,6 +112,7 @@ public class GridActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    //placement initial des pièces j2
     private void initTeamTwo() {
         for (int y=0; y<col_MAX; y++){
             for (int x=0; x<=1; x++){
@@ -104,6 +129,7 @@ public class GridActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    //placement initial des pièces j1
     private void initTeamOne() {
         for (int y=0; y<col_MAX; y++){
             for (int x=row_MAX-2; x<row_MAX; x++){
@@ -120,4 +146,112 @@ public class GridActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    //coordonées vers position liste
+    private int xyToPos(int y, int x){
+        return x*col_MAX+y;
+    }
+
+    //position liste vers coordonée y
+    private int posToY(int pos){
+        return pos%col_MAX;
+    }
+
+    //position liste vers coordonée x
+    private int posToX(int pos){
+        return pos/col_MAX;
+    }
+
+    //fait bouger une pièce d'une case à une autre
+    private void movement(int y, int x, int ny, int nx){
+        if(isMoveOk(y,x,ny,nx)){
+            Log.i("test", "déplacement possible");
+            gridPieces[ny][nx] = new Piece(gridPieces[y][x]);
+            gridPieces[y][x] = new Piece();
+            updateImgItems();
+            gridView.setAdapter(gridAdapter);
+        }
+        else{
+            String pushDir = pushOk(y,x,ny,nx);
+            //move ko mais en poussant des pièces ?
+            if(pushDir.equals("")){
+                Log.i("test", "push possible");
+            }
+        }
+    }
+
+    //vérifie mouvement valide
+    private boolean isMoveOk(int y, int x, int ny, int nx){
+        int gap = xyToPos(ny,nx)-xyToPos(y,x);
+        //case sélectionnée vide
+        if(gridPieces[ny][nx].getTeam()==-1){
+            //si case sélectionnée est autour
+            if(gap==1 || gap==-1 || gap ==col_MAX || gap==-col_MAX){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    //vérifie si on peut pousser les pièces
+    private String pushOk(int y, int x, int ny, int nx){
+        String pushDir = "";
+        nPos = xyToPos(ny,nx);
+        int gap = nPos-xyToPos(y,x);
+        if(gap==1){ //droite
+            while (nPos<xyToPos(ny,nx)+(col_MAX-y-1) && pushDir.equals("")){
+                if(gridPieces[posToY(nPos)][posToX(nPos)].getTeam()==-1){
+                    pushDir = "d";
+                }
+                nPos++;
+            }
+        }
+        else if(gap==-1){ //gauche
+            while (nPos>xyToPos(ny,nx)-y && pushDir.equals("")){
+                if(gridPieces[posToY(nPos)][posToX(nPos)].getTeam()==-1){
+                    pushDir = "g";
+                }
+                nPos--;
+            }
+        }
+        else if(gap==col_MAX){ //bas
+            while (nPos<col_MAX*row_MAX && pushDir.equals("")){
+                if(gridPieces[posToY(nPos)][posToX(nPos)].getTeam()==-1){
+                    pushDir = "b";
+                }
+                nPos=nPos+col_MAX;
+            }
+
+        }
+        else if(gap==-col_MAX) { //haut
+            while (nPos>=0 && pushDir.equals("")){
+                if(gridPieces[posToY(nPos)][posToX(nPos)].getTeam()==-1){
+                    pushDir = "h";
+                }
+                nPos=nPos-col_MAX;
+            }
+        }
+
+        return pushDir;
+    }
+
+    //pousse les pièces dans le sens du mouvement
+    private void pushPieces(int ny, int nx, String pushDir){
+        switch (pushDir){
+            case "d":
+                for (int gap=nPos;gap>nPos-ny;gap--){
+                    gridPieces[posToY(nPos)][posToX(nPos)] = new Piece(gridPieces[posToY(nPos)-1][posToX(nPos)]);
+                }
+                gridPieces[ny-1][nx] = new Piece();
+                break;
+            case "g":
+                break;
+            case "b":
+                break;
+            case "h":
+                break;
+            default:
+                break;
+        }
+    }
 }
--- app/src/main/java/com/teclo/cballini/teclo/Piece.java
+++ app/src/main/java/com/teclo/cballini/teclo/Piece.java
@@ -1,6 +1,7 @@
 package com.teclo.cballini.teclo;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 
@@ -20,6 +21,16 @@ public class Piece {
         row = 0;
         alive = true;
         team = -1;
+        img = Bitmap.createBitmap(24,24,Bitmap.Config.RGB_565);
+        img.eraseColor(Color.WHITE);
+    }
+
+    public Piece(Piece p){
+        col = p.getCol();
+        row = p.getRow();
+        alive = p.isAlive();
+        team = p.getTeam();
+        img = p.getImg();
     }
 
     public int getCol() {
